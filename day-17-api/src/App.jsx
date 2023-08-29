367bda43811dca5ddb83b9789de3543bc33d23a0
jazz <jazxbx@gmail.com>
2023-08-28 18:05:48 -0700

    added memes component

--- day-17-api/src/App.jsx
+++ day-17-api/src/App.jsx
@@ -1,10 +1,12 @@
 import { DogSection } from "./Components/DogSection";
+import { Memes } from "./Components/MemesSection";
 
 function App() {
   return (
     <>
       <h1>Can you catch them all?</h1>
       <DogSection />
+      <Memes />
     </>
   );
 }
---
+++ day-17-api/src/Components/MemesSection.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const Memes = () => {
+  const [memes, setMemes] = useState([]);
+
+  async function fetchMemes() {
+    const res = await fetch("https://api.imgflip.com/get_memes");
+    const info = await res.json();
+    // checking what obj was fetched
+    // console.log(info);
+    setMemes(info.data.memes);
+  }
+
+  useEffect(() => {
+    fetchMemes();
+  }, []);
+
+  return (
+    <>
+      <h2 className="heading">Memes</h2>
+      <p className="text">
+        {`Here's an API to fetch random memes:`}
+        <a href="https://api.imgflip.com/get_memes"></a>
+      </p>
+      <div className="container">
+        {memes.map((meme, id) => (
+          <div key={id}>
+            <img src={meme.url}></img>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export { Memes };
